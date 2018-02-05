2c132c9704cd5d1fa83762b6a5dabd20ce542a33
gorilladoll <gorilladoll@naver.com>
2018-02-05 21:49:19 +0900

    Set Git and Save First practice file

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,22 @@
+<?php
+  namespace App\Controller;
+
+  use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+  use Symfony\Component\HttpFoundation\Response;
+
+  class ArticleController{
+    /**
+      @Route("/")
+    */
+    public function homepage(){
+      return new Response('OMG! My First page already');
+    }
+
+    /**
+     * @Route("/news/{slug}");
+     */
+    public function show($slug){
+      return new Response(sprintf("Future page to show one space article:%s",$slug));
+    }
+  }
+?>
