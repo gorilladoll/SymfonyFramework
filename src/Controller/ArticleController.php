fd91a8c8661dad177ec36e5ac3e8a4f7b2ab6535
gorilladoll <gorilladoll@naver.com>
2018-02-06 23:44:26 +0900

    learn how to download twig and how to use twig

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,9 +2,10 @@
   namespace App\Controller;
 
   use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+  use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
   use Symfony\Component\HttpFoundation\Response;
 
-  class ArticleController{
+  class ArticleController extends AbstractController{
     /**
       @Route("/")
     */
@@ -12,15 +13,29 @@
       return new Response('OMG! My First page already');
     }
 
+
+    // public function show($slug){
+    //   return new Response(
+    //     sprintf(
+    //       "Future page to show one space article:%s",$slug
+    //     )
+    //   );
+    // }
+
     /**
      * @Route("/news/{slug}");
      */
     public function show($slug){
-      return new Response(
-        sprintf(
-          "Future page to show one space article:%s",$slug
-        )
-      );
+      $comments = [
+        'i ate a normal rock one. etc..',
+        'i can diet',
+        'i like chicken'
+      ];
+
+      return $this->render('article/show.html.twig',[
+        'title' => ucwords(str_replace('-',' ',$slug)),
+        'comments' => $comments,
+      ]);
     }
   }
 ?>
